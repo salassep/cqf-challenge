9577c9597e79d1ab563e1af4601f86f38f481fc7
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2024-03-22 18:01:35 +0800

    Update bottom navbar

--- src/components/navbars/BottomNavbar.vue
+++ src/components/navbars/BottomNavbar.vue
@@ -4,26 +4,26 @@ import { Icon } from '@iconify/vue';
 
 <template>
   <nav class="desktop-sm:hidden block fixed bottom-0 right-0 left-0 h-16 bg-white">
-    <div class="wrapper">
-      <ul class="menu flex justify-around">
+    <div class="wrapper h-full flex flex-col justify-center">
+      <ul class="menu flex justify-around text-[#636363]">
         <li>
-          <Icon class="text-2xl mx-auto" icon="akar-icons:home" />
+          <Icon class="text-2xl mx-auto" icon="octicon:home-16" />
           <span class="text-xs">Beranda</span>
         </li>
         <li>
-          <Icon class="text-2xl mx-auto" icon="akar-icons:home" />
+          <Icon class="text-2xl mx-auto" icon="icon-park-outline:oval-love" />
           <span class="text-xs">Donasi</span>
         </li>
         <li>
-          <Icon class="text-2xl mx-auto" icon="akar-icons:home" />
+          <Icon class="text-2xl mx-auto" icon="material-symbols:mosque-outline-rounded" />
           <span class="text-xs">Program</span>
         </li>
         <li>
-          <Icon class="text-2xl mx-auto" icon="akar-icons:home" />
+          <Icon class="text-2xl mx-auto" icon="material-symbols:menu-book-outline" />
           <span class="text-xs">Kajian</span>
         </li>
         <li>
-          <Icon class="text-2xl mx-auto" icon="akar-icons:home" />
+          <Icon class="text-2xl mx-auto" icon="bi:three-dots" />
           <span class="text-xs">Lainnya</span>
         </li>
       </ul>
