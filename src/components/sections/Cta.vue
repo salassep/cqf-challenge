970762d2ff734e636ade92e8dd3ca89dc678f225
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2024-03-28 00:10:57 +0800

    Build dist

--- src/components/sections/Cta.vue
+++ src/components/sections/Cta.vue
@@ -5,7 +5,7 @@ import { Icon } from '@iconify/vue';
 <template>
   <div class="wrapper py-20">
     <div class="relative h-[272px]" data-aos="fade-up">
-      <img class="rounded-3xl object-cover h-full w-full" src="../assets/images/ctas/cta-1.png" alt="Background CTA">
+      <img class="rounded-3xl object-cover h-full w-full" src="../../assets/images/ctas/cta-1.png" alt="Background CTA">
       <div class="cta-gradient rounded-3xl opacity-80"></div>
       <div class="absolute top-0 left-0 desktop-sm:w-[70%] w-full h-full flex flex-col justify-center px-9 gap-6">
         <p class="text-white font-semibold mobile-lg:text-[32px] mobile-sm:text-2xl text-xl mobile-sm:leading-snug leading-normal">Mari bergabung menjadi keluarga Cinta Quran Foundation agar bisa berbagi dengan sesama</p>
